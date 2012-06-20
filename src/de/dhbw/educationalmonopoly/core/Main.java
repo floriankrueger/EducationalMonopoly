9382b9eac2c763cd09c81e6d33eb3acdb5b89390
Ben-G <Benjamin.Encz@gmail.com>
2012-06-20 18:34:49 +0300

    Temporarily removed startup mechanism

--- src/de/dhbw/educationalmonopoly/core/Main.java
+++ src/de/dhbw/educationalmonopoly/core/Main.java
@@ -21,59 +21,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// create game
-		Game game = GameFactory.createGameWithType(MonopolyType.CLASSIC);
-
-		// extract dimensional values from the strings array
-		int height = -1;
-		int width = -1;
-
-		try {
-			
-			// parse width
-			if (args.length == 1) {
-				
-				// parse width
-				width = Integer.parseInt(args[0]);
-				
-				// calculate heigth
-				height = (int)(width * 0.75);
-				
-			}
-
-			// parse width & height
-			else if (args.length == 2) {
-
-				// parse width
-				width = Integer.parseInt(args[0]);
-
-				// parse height
-				height = Integer.parseInt(args[1]);
-
-			}
-
-		} catch (NumberFormatException nfe) {
-
-			System.out.println("Unsupported Command Line Parameters (2). Ignoring.");
-
-		}
-
-		// create swing ui representation
-		IGameRepresentation gameRepresentation;
-
-		if ((width != -1) && (height != -1)) {
-			gameRepresentation = new SwingGameRepresentation(width, height);
-		} else {
-			gameRepresentation = new SwingGameRepresentation();
-		}
-		game.setGameRepresenation(gameRepresentation);
-
-		// create two players
-		game.addNewLocalPlayer("Bugs Bunny");
-		game.addNewLocalPlayer("Duffy Duck");
-
-		// start the game
-		game.start();
+		// FIXME: Aufgabe 1, Spiel über Konsolenmenü starten
 	}
 
 }
